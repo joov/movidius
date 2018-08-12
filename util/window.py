1df581d82c5124c0d36150da0ba8ec17cd39deb9
Chen-Ting Chuang <justin.chuang@gmail.com>
2018-08-12 16:01:47 +0800

    Basic FPS

--- util/window.py
+++ util/window.py
@@ -19,18 +19,37 @@
 # SOFTWARE.
 
 import cv2
+import time
 
 
 _WINDOW = 'preview'
 
 
+def _current_millis():
+    return int(round(time.time() * 1000))
+
+
 class Window(object):
+    def __init__(self, show_fps=True):
+        self.show_fps = show_fps
+        self.last_time = _current_millis()
+        self.frame_count = 0
+
     def open(self):
         cv2.namedWindow(_WINDOW, cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty(_WINDOW, cv2.WND_PROP_FULLSCREEN,
                               cv2.WINDOW_FULLSCREEN)
 
     def show_frame(self, frame):
+        if self.show_fps:
+            now = _current_millis()
+            if (now - self.last_time) > 1000:
+                fps = float(self.frame_count) / (now - self.last_time) * 1000.0
+                print('fps: %.2f' % fps)
+                self.last_time = now
+                self.frame_count = 0
+            else:
+                self.frame_count += 1
         cv2.imshow(_WINDOW, frame)
 
     def close(self):
