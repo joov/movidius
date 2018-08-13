f9be52046803549d2a840b943565946f5826dfda
Chen-Ting Chuang <justin.chuang@gmail.com>
2018-08-13 09:59:53 +0800

    Fix a typo in DISPLAY check

--- util/environ_check.py
+++ util/environ_check.py
@@ -5,4 +5,4 @@ assert sys.version_info.major >= 3, 'Error: please use python3.'
 
 assert os.environ.get('DISPLAY', None), (
     "Error: DISPLAY is not set. \n" +
-    "Run 'export DISPLAY=0.0' if you're SSHing to Raspberry Pi")
+    "Run 'export DISPLAY=:0.0' if you're SSHing to Raspberry Pi")
